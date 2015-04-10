52eb61c15daf5967c65b38df71c8f710fa0d6949
aucfan-makise <makise@aucfan.com>
2015-04-10 16:18:24 +0900

    echoでタグを出力しないようにした

--- CalendarPrinter.php
+++ CalendarPrinter.php
@@ -15,20 +15,24 @@ class CalendarPrinter{
 	}
 	
 	public function printCalendar(){
-		echo '
-			<table class="calendar_table">
-			<tr><td class="calendar_table_title" colspan="7">'.
-			$this->year."年".$this->month."月".'</td></tr>
+		$tableTitle = $this->year."年".$this->month."月";
+?>
+		<table class="calendar_table">
+			<tr>
+				<td class="calendar_table_title" colspan="7">
+					<?php echo $tableTitle ?>
+				</td>
+			</tr>
 			<tr class="calendar_week_row">
-			<td class="calendar_week_column">Sun</td>
-			<td class="calendar_week_column">Mon</td>
-			<td class="calendar_week_column">The</td>
-			<td class="calendar_week_column">Wed</td>
-			<td class="calendar_week_column">Thu</td>
-			<td class="calendar_week_column">Fri</td>
-			<td class="calendar_week_column">Sat</td>
-			</tr>';
-		
+				<td class="calendar_week_column">Sun</td>
+				<td class="calendar_week_column">Mon</td>
+				<td class="calendar_week_column">The</td>
+				<td class="calendar_week_column">Wed</td>
+				<td class="calendar_week_column">Thu</td>
+				<td class="calendar_week_column">Fri</td>
+				<td class="calendar_week_column">Sat</td>
+			</tr>
+<?php	
 // 		月の日の開始の位置
 		$firstWeekDay = $this->getFirstWeekDay($year, $month);
 		$firstColumnNum = 1 - $firstWeekDay;
@@ -42,39 +46,48 @@ class CalendarPrinter{
 	}
 	
 	private function printEndCalendar(){
-		echo '</table>';
+?>
+		</table>
+<?php 
 	}
 	
 	private function printDayRow($start, $end){
 		$publicHolidayData = new PublicHolidayRequester($this->year, $this->month);
-		echo '<tr class="calendar_day_row">';
-		for ($i = $start; $i <= $end; ++$i){
-			if ($i === $this->today)
-				echo '<td class="calendar_today_column">';
-			elseif ($publicHolidayData->isHoliday($i))
-				echo '<td class="calendar_public_holiday_column">';
-			elseif ($i === $start)
-				echo '<td class="calendar_sunday_column">';
-			elseif ($i === $start + 6)
-				echo '<td class="calendar_saturday_column">';
-			else
-				echo '<td class="calendar_day_column">';
-			
+?>
+			<tr class="calendar_day_row">
+<?php	for ($i = $start; $i <= $end; ++$i){ ?>
+<?php		if ($i === $this->today) {?>
+				<td class="calendar_today_column">
+<?php		} elseif ($publicHolidayData->isHoliday($i)) {?>
+				<td class="calendar_public_holiday_column">
+<?php		} elseif ($i === $start) {?>
+				<td class="calendar_sunday_column">
+<?php		} elseif ($i === $start + 6) {?>
+				<td class="calendar_saturday_column">
+<?php		} else {?>
+				<td class="calendar_day_column">
+<?php		}
 			if($i > 0 && $i <= $this->lastDay)
 				echo $i;
 			if($publicHolidayData->isHoliday($i))
 				echo " ".$publicHolidayData->getHolidayName($i);
-			echo '</td>';
-		}
-		echo	'</tr>';
+?>
+				</td>
+<?php	} ?>
+			</tr>
+<?php 
 		$this->printMessageColumn();		
 	}
+	
 	private function printMessageColumn(){
-		echo '<tr>';
-		for ($i = 0; $i < 7; ++$i)
-			echo '<td class="calendar_nomal_column">test</td>';
-		echo '</tr>';
-	}
+?>
+			<tr>
+<?php for ($i = 0; $i < 7; ++$i) {?>
+				<td class="calendar_nomal_column">test</td>
+<?php }?>
+			</tr>
+<?php }
+
 	private function getFirstWeekDay(){
 		return date("w", strtotime($this->year."-".$this->month."-01"));
 	}
--- calendar.php
+++ calendar.php
@@ -1,3 +1,8 @@
+<?php 
+	require "CalendarData.php";
+	require "CurrentCalendarData.php";
+	require "CalendarPrinter.php";
+?>
 <html>
 	<head>
 		<title>Calendar</title>
@@ -5,53 +10,61 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	</head>
 	<body>
-	
-	<?php 
-	require "CalendarData.php";
-	require "CurrentCalendarData.php";
-	require "CalendarPrinter.php";
-
+<?php 
 	$currentCalendarData = new CurrentCalendarData();
-		$centerCalendarData = new CurrentCalendarData();
-		
-// 		次・前のカレンダーの遷移操作
-		if($_GET["pre"] != null)
-			$centerCalendarData = new CalendarData($_GET["pre"]);
-		elseif($_GET["next"] != null)
-			$centerCalendarData = new CalendarData($_GET["next"]);
-		elseif($_GET["date_select"] != null)
-			$centerCalendarData = new CalendarData($_GET["date_select"]);
-		else
-			$centerCalendarData = new CalendarData($currentCalendarData->getYear()."-".$currentCalendarData->getMonth());
+	$centerCalendarData = new CurrentCalendarData();
+	
+// 	次・前のカレンダーの遷移操作
+	if($_GET["pre"] != null)
+		$centerCalendarData = new CalendarData($_GET["pre"]);
+	elseif($_GET["next"] != null)
+		$centerCalendarData = new CalendarData($_GET["next"]);
+	elseif($_GET["date_select"] != null)
+		$centerCalendarData = new CalendarData($_GET["date_select"]);
+	else
+		$centerCalendarData = new CalendarData($currentCalendarData->getYear()."-".$currentCalendarData->getMonth());
+?>
+		<form method="get" action="calendar.php">
+			<button name="pre" value="
+<?php echo date('Y-m', strtotime($centerCalendarData->getYear()."-".$centerCalendarData->getMonth()." -1 month")) ?>
+			">前</button>
+			<button name="next" value="
+<?php echo date('Y-m', strtotime($centerCalendarData->getYear()."-".$centerCalendarData->getMonth()." +1 month")) ?>
+			">次</button>
 
-		echo '
-			<form method="get" action="calendar.php">
-				<button name="pre" value="'.date("Y-m", strtotime($centerCalendarData->getYear()."-".$centerCalendarData->getMonth()." -1 month")).'">前</button>
-				<button name="next" value="'.date("Y-m", strtotime($centerCalendarData->getYear()."-".$centerCalendarData->getMonth()." +1 month")).'">次</button>';
-// 		コンボボックス
-		echo '<p><select name="date_select">';
+<!-- コンボボックス -->
+			<p>
+				<select name="date_select">
+<?php 
 		for($i = -10; $i <= 10; ++$i){
 			list($year, $month) = $currentCalendarData->getCalcMonth($i);
-			if ($centerCalendarData->getYear() == $year && $centerCalendarData->getMonth() == $month)
-				echo '<option value="'.$year.'-'.$month.'" selected>'.$year."年".$month."月</option>";
-			else
-				echo '<option value="'.$year.'-'.$month.'">'.$year."年".$month."月</option>";
-		}
-		echo '</select><input type="submit" value="select"></p></form>';
-		
-		echo '<table border="0"><tr>';
+			$value = $year."-".$month;
+			$output = $year."年".$month."月";
+			if ($centerCalendarData->getYear() == $year && $centerCalendarData->getMonth() == $month){
+?>
+				<option value="<?php echo $value ?>" selected><?php echo $output ?></option>
+<?php 		} else ?>
+				<option value="<?php echo $value ?>"><?php echo $output ?></option>
+<?php	} ?>
+ 
+				</select>
+				<input type="submit" value="select">
+			</p>
+		</form>
+		<table>
+			<tr>
+<?php		
 		for ($i = -1; $i < 2; ++$i){
 			$calc = $centerCalendarData->getCalcMonth($i);
 			if ($calc[0] == $currentCalendarData->getYear() && $calc[1] == $currentCalendarData->getMonth())
 				$printer = new CalendarPrinter($calc[0], $calc[1], $currentCalendarData->getDay());
 			else 
 				$printer = new CalendarPrinter($calc[0], $calc[1]);
-			echo '<td>';
-			$printer->printCalendar();
-			echo '</td>';
-		}
-		echo '</table>'
-		
 ?>
+				<td>
+<?php		$printer->printCalendar() ?>
+				</td>
+<?php 	} ?>
+		</table>
 	</body>
 </html>
