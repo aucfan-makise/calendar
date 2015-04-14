830891fe4c39cd413c598c21e7ea076d53f7cc6a
aucfan-makise <makise@aucfan.com>
2015-04-14 19:25:08 +0900

    コードレビューの修正を行った。

--- calendar.php
+++ calendar.php
@@ -1,88 +1,164 @@
 <?php 
-	require "CalendarData.php";
-	require "CurrentCalendarData.php";
-	require "CalendarPrinter.php";
+    require "CalendarFunction.php";
+
+//     初期化 カレンダーの数
+    $calendar_num = 3;
+    
+//     今日の日付
+    $today;
+    $today = date("Y-m-d");
+    
+    //     表示するカレンダーの年月 Y-m
+    $center_calendar;
+    if(! is_null($_GET["pre"])) $center_calendar = $_GET["pre"];
+    elseif(! is_null($_GET["next"])) $center_calendar = $_GET["next"];
+    elseif(! is_null($_GET["date_select"])) $center_calendar = $_GET["date_select"];
+    else $center_calendar = date("Y-m");
+    
+    //     カレンダーの数
+    if(! is_null($_GET["calendarNum"])) $calendar_num = $_GET["calendarNum"];
+//     カレンダーのループの開始と終わり
+    $start_calendar = date("Y-n", strtotime($center_calendar." -".floor($calendar_num / 2)." month"));
+    list($start_year, $start_month) = explode("-", $start_calendar);
+    $end_calendar = date("Y-n", strtotime($center_calendar." +".ceil($calendar_num /2)." month -1 month"));
+    list($end_year, $end_month) = explode("-", $end_calendar);
+    
+    $calendar_function = new CalendarFunction();
+    $calendar_function->createCarendarArray($start_year, $start_month, $end_year, $end_month);
+    $calendar_array = $calendar_function->getCarendarArray();
+    
+    $calendar_td = array("default" => "calendar_day_column", "0" => "calendar_sunday_column", "6" => "calendar_saturday_column", "public_holiday" => "calendar_public_holiday_column")
 ?>
 <html>
-	<head>
-		<title>Calendar</title>
-		<link rel="stylesheet" type="text/css" href="./calendar.css">
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	</head>
-	<body>
-<?php 
-	$calendarNum = 3;
-	$currentCalendarData = new CurrentCalendarData();
-	$centerCalendarData = new CurrentCalendarData();
-	
-// 	次・前のカレンダーの遷移操作
-	if($_GET["pre"] != null)
-		$centerCalendarData = new CalendarData($_GET["pre"]);
-	elseif($_GET["next"] != null)
-		$centerCalendarData = new CalendarData($_GET["next"]);
-	elseif($_GET["date_select"] != null)
-		$centerCalendarData = new CalendarData($_GET["date_select"]);
-	else
-		$centerCalendarData = new CalendarData($currentCalendarData->getYear()."-".$currentCalendarData->getMonth());
-	
-// 	カレンダーの数
-	if($_GET["calendarNum"] != null)
-		$calendarNum = $_GET["calendarNum"];
-?>
-		<form method="get" action="calendar.php">
-			<button name="pre" value="
-<?php echo date('Y-m', strtotime($centerCalendarData->getYear()."-".$centerCalendarData->getMonth()." -1 month")) ?>
-			">前</button>
-			<button name="next" value="
-<?php echo date('Y-m', strtotime($centerCalendarData->getYear()."-".$centerCalendarData->getMonth()." +1 month")) ?>
-			">次</button>
-
-<!-- コンボボックス -->
-			<p>
-				<select name="date_select">
-<?php 
-		for($i = -10; $i <= 10; ++$i){
-			list($year, $month) = $currentCalendarData->getCalcMonth($i);
-			$value = $year."-".$month;
-			$output = $year."年".$month."月";
-			if ($centerCalendarData->getYear() == $year && $centerCalendarData->getMonth() == $month){
-?>
-				<option value="<?php echo $value ?>" selected><?php echo $output ?></option>
-<?php 		} else ?>
-				<option value="<?php echo $value ?>"><?php echo $output ?></option>
-<?php	} ?>
-				</select>
-				<input type="submit" value="select">
-			</p>
-			<p>
-			表示するカレンダーの数
-				<input type="text" size=2 maxlength="2" name="calendarNum" value="<?php echo $calendarNum ?>">
-				<input type= "submit" value="change">
-			</p>
-		</form>
-		<table>
-			<tr>
-<?php		
-// カレンダーの表示
-		$calendarLoopStart = "-".floor($calendarNum / 2);
-		$calendarLoopEnd = ceil($calendarNum / 2);
-		for ($i = $calendarLoopStart; $i < $calendarLoopEnd; ++$i){
-			$calc = $centerCalendarData->getCalcMonth($i);
-			if ($calc[0] == $currentCalendarData->getYear() && $calc[1] == $currentCalendarData->getMonth())
-				$printer = new CalendarPrinter($calc[0], $calc[1], $currentCalendarData->getDay());
-			else 
-				$printer = new CalendarPrinter($calc[0], $calc[1]);
-?>
-				<td>
-<?php		$printer->printCalendar() ?>
-				</td>
-				
-<?php 		if(($calendarLoopStart - $i + 2) % 3 == 0){ ?>
-			</tr>
-			<tr>
-<?php }?>
-			
-<?php 	} ?>
-		</table>
-	</body>
+    <head>
+        <title>Calendar</title>
+        <link rel="stylesheet" type="text/css" href="./calendar.css">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+    <body>
+        <form method="get" action="CopyOfcalendar.php">
+            <button name="pre" value="<?php echo date('Y-m', strtotime($center_calendar." -1 month"))?>">前</button>
+            <button name="next" value="<?php echo date('Y-m', strtotime($center_calendar." +1 month"))?>">次</button>
+            <!-- コンボボックス -->
+            <p>
+                <select name="date_select">
+                    <?php foreach (CalendarFunction::getComboBoxArray(date("Y-n", strtotime($today))) as $key => $value):?>
+                        <?php if ($key === $center_calendar):?>
+                    <option value="<?php echo $key ?>" selected><?php echo $value ?></option>
+                        <?php else: ?>
+                    <option value="<?php echo $key ?>"><?php echo $value ?></option>
+                        <?php endif; ?>
+                    <?php endforeach;?>
+                </select>
+                <input type="submit" value="select">
+            </p>
+            <p>
+            表示するカレンダーの数
+                <input type="text" size=2 maxlength="2" name="calendarNum" value="<?php echo $calendar_num ?>">
+                <input type= "submit" value="change">
+            </p>
+        </form>
+        <table><tr>
+            <?php for ($printing = $start_calendar; strtotime($printing) <= strtotime($end_calendar); $printing = date("Y-n", strtotime($printing." + 1 month"))):?>
+                <?php list($year, $month) = explode("-", $printing)?>
+                <?php if (date("n", strtotime($printing." - ".$start_calendar)) % 3 === 0):?>
+                </tr><tr>
+                <?php endif;?>
+                <td><table class="calendar_table">
+                    <tr>
+                        <td class="calendar_table_title" colspan="7">
+                            <?php echo $year."年".$month."月"?>
+                        </td>
+                    </tr>
+                    <tr class="calendar_week_row">
+        				<td class="calendar_week_column">Sun</td>
+        				<td class="calendar_week_column">Mon</td>
+        				<td class="calendar_week_column">Tue</td>
+        				<td class="calendar_week_column">Wed</td>
+        				<td class="calendar_week_column">Thu</td>
+        				<td class="calendar_week_column">Fri</td>
+        				<td class="calendar_week_column">Sat</td>
+        			</tr>
+<!--         			前月 -->
+                        <?php for ($before_month = CalendarFunction::getBeforeMonth($printing), $day = date("t", strtotime($year."-".$month." -1 month")) - $calendar_array[$year][$month][1]["week_day"] + 1; $day <= date("t", strtotime($year."-".$month." -1 month")); ++$day):?>
+<?php switch ($calendar_array[$before_month['year']][$before_month['month']][$day]["week_day"]):?>
+<?php case 0:?>
+<pre><?php echo $calendar_array[$before_month['year']][$before_month['month']][$day]["week_day"]?></pre>
+<pre><?php echo $before_month['year']." ".$before_month['month']." ".$day?></pre>
+<tr class="calendar_day_row">
+<?php if (! empty($calendar_array[$before_month['year']][$before_month['month']][$day]["holiday_name"])):?>
+    <td class=<?php $calendar_td["public_holiday"]?>><?php echo $day." ".$calendar_array[$before_month['year']][$before_month['month']][$day]["holiday_name"]?></td>
+<?php else:?>
+    <td class=<?php echo $calendar_td["0"]?>><?php echo $day?></td>
+<?php endif;?>
+<?php break;?>
+<?php default:?>
+<?php if (! empty($calendar_array[$before_month['year']][$before_month['month']][$day]["holiday_name"])):?>
+    <td class=<?php echo $calendar_td["public_holiday"]?>><?php echo $day." ".$calendar_array[$before_month['year']][$before_month['month']][$day]["holiday_name"]?></td>
+<?php else:?>
+    <td class=<?php echo $calendar_td["default"]?>><?php echo $day?></td>
+<?php endif;?>
+<?php endswitch;?>
+                        <?php endfor;?>
+<!--         			今月 -->
+                        <?php for ($day = 1; $day <= date('t', strtotime($year."-".$month)); ++$day):?>
+<?php switch ($calendar_array[$year][$month][$day]["week_day"]):?>
+<?php case 0:?>
+<tr class="calendar_day_row">
+<?php if (! empty($calendar_array[$year][$month][$day]["holiday_name"])):?>
+    <td class=<?php echo $calendar_td["public_holiday"]?>><?php echo $day." ".$calendar_array[$year][$month][$day]["holiday_name"] ?></td>
+<?php else:?>
+    <td class=<?php echo $calendar_td["0"]?>><?php echo $day?></td>
+<?php endif;?>
+<?php break;?>
+<?php case 6:?>
+<?php if(! empty($calendar_array[$year][$month][$day]["holiday_name"])):?>
+    <td class=<?php echo $calendar_td["public_holiday"]?>><?php echo $day." ".$calendar_array[$year][$month][$day]["holiday_name"] ?></td>
+<?php else:?>
+    <td class=<?php echo $calendar_td["6"]?>><?php echo $day?></td>
+<?php endif;?>
+</tr>
+<tr>
+<?php for ($i = 0; $i < 7; ++$i):?>
+    <td class="calendar_nomal_column">test</td>
+<?php endfor;?>
+</tr>
+<?php break;?>
+<?php default:?>
+<?php if (! empty($calendar_array[$year][$month][$day]["holiday_name"])):?>
+    <td class=<?php echo $calendar_td["public_holiday"]?>><?php echo $day." ".$calendar_array[$year][$month][$day]["holiday_name"]?></td>
+<?php else:?>
+    <td class=<?php echo $calendar_td["default"]?>><?php echo $day?></td>
+<?php endif;?>
+<?php endswitch;?>
+                        <?php endfor;?>
+<!--                         翌月 -->
+                        <?php for ($next_month = CalendarFunction::getNextMonth($printing), $day = 1, $i = $calendar_array[$next_month["year"]][$next_month["month"]][1]["week_day"]; $i != 0 && $i <= 6; ++$day, ++$i):?>
+<?php switch ($calendar_array[$next_month["year"]][$next_month["month"]][$day]["week_day"]):?>
+<?php case 6:?>
+<?php if(! empty($calendar_array[$next_month["year"]][$next_month["month"]][$day]["holiday_name"])):?>
+    <td class=<?php echo $calendar_td["public_holiday"]?>> <?php echo $day." ".$calendar_array[$next_month["year"]][$next_month["month"]][$day]["holiday_name"]?></td>
+<?php else:?>
+    <td class=<?php echo $calendar_td["6"]?>> <?php echo $day ?></td>
+<?php endif;?>
+</tr>
+<tr>
+<?php for ($i = 0; $i < 7; ++$i):?>
+    <td class="calendar_nomal_column">test</td>
+<?php endfor;?>
+</tr>
+<?php break;?>
+<?php default:?>
+<?php if (! empty($calendar_array[$next_month["year"]][$next_month["month"]][$day]["holiday_name"])):?>
+    <td class=<?php echo $calendar_td["public_holiday"]?>><?php echo $day." ".$calendar_array[$next_month["year"]][$next_month["month"]][$day]["holiday_name"]?></td>
+<?php else:?>
+    <td class=<?php echo $calendar_td["default"]?>><?php echo $day?></td>
+<?php endif;?>
+<?php endswitch;?>
+                        <?php endfor;?>
+                        </table></td>
+            <?php endfor;?>
+            </tr>
+        </table>
+    </body>
 </html>
